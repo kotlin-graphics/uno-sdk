b6754ed111670141423aff3080de7977385475fe
Giuseppe Barbieri <elect86@gmail.com>
2017-01-27 19:42:00 +0100

    - ShaderProgram

--- src/main/kotlin/extensions/Extensions.kt
+++ src/main/kotlin/extensions/Extensions.kt
@@ -14,3 +14,30 @@ val ShaderProgram.name
 
 fun ShaderCode.create(gl: GL2ES2, type: Int, context: Class<*>, sourceFiles: Array<String>)
         = ShaderCode.create(gl, type, sourceFiles.size, context, sourceFiles, false)
+
+fun createShaderProgram(gl: GL2ES2, context: Class<*>, shaderSrc: String): ShaderProgram {
+
+    val program = ShaderProgram()
+
+    val vertShader = ShaderCode.create(gl, GL2ES2.GL_VERTEX_SHADER, 1, context, arrayOf("$shaderSrc.vert"), false)
+    val fragShader = ShaderCode.create(gl, GL2ES2.GL_FRAGMENT_SHADER, 1, context, arrayOf("$shaderSrc.frag"), false)
+
+    program.add(vertShader)
+    program.add(fragShader)
+
+    program.init(gl)
+
+    program.link(gl, System.err)
+
+    vertShader.destroy(gl)
+    fragShader.destroy(gl)
+
+    return program
+}
+
+class ShaderProgramUtil() {
+    companion object {
+        @JvmStatic fun create(gl: GL2ES2, context: Class<*>, shaderSrc: String): ShaderProgram =
+                createShaderProgram(gl, context, shaderSrc)
+    }
+}
