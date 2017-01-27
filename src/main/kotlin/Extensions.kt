faa3e4b105586ee19cb0752c79136b11e483b033
Giuseppe Barbieri <elect86@gmail.com>
2017-01-27 17:51:51 +0100

    - ShaderProgram.name

--- src/main/kotlin/Extensions.kt
+++ src/main/kotlin/Extensions.kt
@@ -1,4 +1,5 @@
 import com.jogamp.common.net.Uri
+import com.jogamp.opengl.util.glsl.ShaderProgram
 
 /**
  * Created by GBarbieri on 27.01.2017.
@@ -6,3 +7,6 @@ import com.jogamp.common.net.Uri
 
 val String.uri
     get() = Uri.valueOf(javaClass.getResource(this))
+
+val ShaderProgram.name
+    get() = program()
