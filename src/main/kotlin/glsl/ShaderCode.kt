f6b1dcefa522dd37c324e936e0ff1bf38b05628f
Giuseppe Barbieri <elect86@gmail.com>
2017-02-21 17:19:28 +0100

    maybe^2

--- src/main/kotlin/glsl/ShaderCode.kt
+++ src/main/kotlin/glsl/ShaderCode.kt
@@ -9,6 +9,15 @@ import com.jogamp.opengl.util.glsl.ShaderCode
  * Created by GBarbieri on 21.02.2017.
  */
 
+fun shaderCodeOf(string: String, gl: GL2ES2, context: Class<*>): ShaderCode =
+        ShaderCode.create(gl, string.type, context,
+                if (string.contains('/')) string.substringBeforeLast('/') else "",  // root
+                null,
+                string.substringAfterLast('/').substringBefore('.'),    // src
+                string.substringAfterLast('.'), // extension
+                null, true)
+
+
 /**
  * https://www.khronos.org/opengles/sdk/tools/Reference-Compiler/
  *
@@ -17,25 +26,14 @@ import com.jogamp.opengl.util.glsl.ShaderCode
  * .tese - a tessellation evaluation shader
  * .geom - a geometry shader
  * .frag - a fragment shader
- * .comp - a compute shader
- * */
-fun shaderCodeOf(string: String, gl: GL2ES2, context: Class<*>): ShaderCode =
-        ShaderCode.create(
-                gl,
-                when (string.substringAfterLast('.')) { // type
-                    "vert" -> GL_VERTEX_SHADER
-                    "tesc" -> GL_TESS_CONTROL_SHADER
-                    "tese" -> GL_TESS_EVALUATION_SHADER
-                    "geom" -> GL_GEOMETRY_SHADER
-                    "frag" -> GL_FRAGMENT_SHADER
-                    "comp" -> GL_COMPUTE_SHADER
-                    else -> throw Error("invalid shader extension")
-                },
-                context,
-                if (string.contains('/')) string.substringBeforeLast('/') else "", // root
-                null, // binary root
-                string.substringAfterLast('/').substringBefore('.'), // src
-                string.substringAfterLast('.'), // extension
-                null, // binary extension
-                true)   // mutable builder
-
+ * .comp - a compute shader     */
+internal val String.type
+    get() = when (substringAfterLast('.')) {
+        "vert" -> GL_VERTEX_SHADER
+        "tesc" -> GL_TESS_CONTROL_SHADER
+        "tese" -> GL_TESS_EVALUATION_SHADER
+        "geom" -> GL_GEOMETRY_SHADER
+        "frag" -> GL_FRAGMENT_SHADER
+        "comp" -> GL_COMPUTE_SHADER
+        else -> throw Error("invalid shader extension")
+    }
