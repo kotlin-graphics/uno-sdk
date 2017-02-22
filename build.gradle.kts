a99404150525effa6fe5324c3448e0fb510a1a10
Giuseppe Barbieri <elect86@gmail.com>
2017-02-22 17:00:42 +0100

    gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ buildscript {
 
     repositories {
         gradleScriptKotlin()
-        maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap-1.1") }
+//        maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap-1.1") }
     }
 
     dependencies {
@@ -57,7 +57,7 @@ dependencies {
 allprojects {
     repositories {
         maven { setUrl("https://jitpack.io") }
-        maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap-1.1") }
+//        maven { setUrl("http://dl.bintray.com/kotlin/kotlin-eap-1.1") }
     }
 }
 
