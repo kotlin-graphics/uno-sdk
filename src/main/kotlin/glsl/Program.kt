8eac2eaea81759591b461ebdeeab09c4669d8659
elect86 <elect86@gmail.com>
2017-02-28 22:49:35 +0100

    - matrixstack rotate rad

--- src/main/kotlin/glsl/Program.kt
+++ src/main/kotlin/glsl/Program.kt
@@ -9,7 +9,6 @@ import com.jogamp.common.net.Uri
 import com.jogamp.opengl.GL2ES2.GL_FRAGMENT_SHADER
 import com.jogamp.opengl.GL2ES2.GL_VERTEX_SHADER
 import com.jogamp.opengl.GL3
-import com.jogamp.opengl.GLException
 import com.jogamp.opengl.util.glsl.ShaderCode
 import com.jogamp.opengl.util.glsl.ShaderProgram
 
