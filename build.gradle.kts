fb206d05e34951044335d7a925740dbd1b65a31b
Giuseppe Barbieri <elect86@gmail.com>
2017-03-09 13:04:47 +0100

    updated glm

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
 
     testCompile("com.github.elect86:kotlintest:d8878d6da0944ec6bcbcdad6a1540bba021d768d")
 
-    compile("com.github.elect86:glm:a9d2fbe13b4246c1e616594f6e0d8a4e301652ba")
+    compile("com.github.elect86:glm:69ba5a9002a2f14ecd71d1f9b531c83053a11656")
 
     val jogl = "2.3.2"
 
