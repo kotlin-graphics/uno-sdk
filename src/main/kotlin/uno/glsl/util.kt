868a7ab7cb8bf36b7171a8de05861c065c810b77
Giuseppe Barbieri <elect86@gmail.com>
2017-03-29 10:32:24 +0200

    base programOf root + string array
    provide all possibilities up to 5 single shaders

--- src/main/kotlin/uno/glsl/util.kt
+++ src/main/kotlin/uno/glsl/util.kt
@@ -7,15 +7,20 @@ import com.jogamp.opengl.util.glsl.ShaderProgram
  * Created by elect on 05/03/17.
  */
 
-@JvmOverloads fun programOf(gl: GL2ES2, context: Class<*>, vararg strings: String, bindAttribLocation: Map<String, Int> = emptyMap()): Int {
+//@JvmOverloads fun programOf(gl: GL2ES2, context: Class<*>, vararg strings: String, bindAttribLocation: Map<String, Int> = emptyMap()): Int {
+//
+//    val shaders =
+//            if (strings[0].contains('.'))
+//                strings.toList()
+//            else {
+//                val root = if (strings[0].endsWith('/')) strings[0] else strings[0] + '/'
+//                strings.drop(1).map { root + it }
+//            }
+//
+//
+//}
 
-    val shaders =
-            if (strings[0].contains('.'))
-                strings.toList()
-            else {
-                val root = if (strings[0].endsWith('/')) strings[0] else strings[0] + '/'
-                strings.drop(1).map { root + it }
-            }
+@JvmOverloads fun programOf(gl: GL2ES2, context: Class<*>, root: String, shaders: Array<String>, bindAttribLocation: Map<String, Int> = emptyMap()): Int {
 
     val shaderProgram = ShaderProgram()
     shaderProgram.init(gl)
@@ -39,3 +44,23 @@ import com.jogamp.opengl.util.glsl.ShaderProgram
 
     return name
 }
+
+@JvmOverloads fun programOf(gl: GL2ES2, context: Class<*>, root: String,
+              shaderA: String, shaderB: String,
+              bindAttribLocation: Map<String, Int> = emptyMap()) =
+        programOf(gl, context, root, arrayOf(shaderA, shaderB))
+
+@JvmOverloads fun programOf(gl: GL2ES2, context: Class<*>, root: String,
+              shaderA: String, shaderB: String, shaderC: String,
+              bindAttribLocation: Map<String, Int> = emptyMap()) =
+        programOf(gl, context, root, arrayOf(shaderA, shaderB, shaderC))
+
+@JvmOverloads fun programOf(gl: GL2ES2, context: Class<*>, root: String,
+              shaderA: String, shaderB: String, shaderC: String, shaderD: String,
+              bindAttribLocation: Map<String, Int> = emptyMap()) =
+        programOf(gl, context, root, arrayOf(shaderA, shaderB, shaderC, shaderD))
+
+@JvmOverloads fun programOf(gl: GL2ES2, context: Class<*>, root: String,
+              shaderA: String, shaderB: String, shaderC: String, shaderD: String, shaderE: String,
+              bindAttribLocation: Map<String, Int> = emptyMap()) =
+        programOf(gl, context, root, arrayOf(shaderA, shaderB, shaderC, shaderD, shaderE))
--- src/test/java/main/Prova.java
+++ src/test/java/main/Prova.java
@@ -2,6 +2,8 @@ package main;
 
 import uno.glm.MatrixStack;
 
+import static uno.glsl.UtilKt.programOf;
+
 /**
  * Created by elect on 15/02/17.
  */
@@ -9,5 +11,6 @@ public class Prova {
 
     public static void main(String[] argvs) {
         MatrixStack matrixStack = new MatrixStack();
+//        int a = programOf()
     }
 }
