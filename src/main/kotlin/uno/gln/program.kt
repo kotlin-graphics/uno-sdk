4b1848ae16f359a485621cfb472d408a199d5d7c
Giuseppe Barbieri <elect86@gmail.com>
2017-04-28 20:32:21 +0200

    program

--- src/main/kotlin/uno/gln/program.kt
+++ src/main/kotlin/uno/gln/program.kt
@@ -7,49 +7,30 @@ import glm.vec3.Vec3
 import glm.vec4.Vec4
 import org.lwjgl.opengl.ARBUniformBufferObject.glGetUniformBlockIndex
 import org.lwjgl.opengl.ARBUniformBufferObject.glUniformBlockBinding
-import org.lwjgl.opengl.GL20.*
+import org.lwjgl.opengl.GL20
 import org.lwjgl.opengl.GL31
 import uno.buffer.byteBufferOf
 import uno.buffer.destroy
 import uno.glsl.Program
 
-fun glCreatePrograms(ints: IntArray) = repeat(ints.size) { ints[it] = glCreateProgram() }
+fun glCreatePrograms(ints: IntArray) = repeat(ints.size) { ints[it] = GL20.glCreateProgram() }
 
 fun initPrograms(ints: IntArray, block: Programs.() -> Unit) {
-    repeat(ints.size) { ints[it] = glCreateProgram() }
+    repeat(ints.size) { ints[it] = GL20.glCreateProgram() }
     Programs.names = ints
     Programs.block()
-}
-
-fun glGetProgram(program: Int, pname: Int): Int {
-    glGetProgramiv(program, pname, intBuffer)
-    return intBuffer[0]
-}
-
-fun glGetProgramInfoLog(program: Int): String {
-
-    glGetProgramiv(program, GL_INFO_LOG_LENGTH, intBuffer)
-    val infoLogLength = intBuffer[0]
-
-    val bufferInfoLog = byteBufferOf(infoLogLength)
-    glGetProgramInfoLog(program, intBuffer, bufferInfoLog)
-
-    val bytes = ByteArray(infoLogLength)
-    bufferInfoLog.get(bytes).destroy()
-
-    return String(bytes)
 }
 
 inline fun usingProgram(program: Int = 0, block: ProgramUse.() -> Unit) {
     ProgramUse.name = program //glUse
     ProgramUse.block()
-    glUseProgram(0)
+    GL20.glUseProgram(0)
 }
 
 inline fun usingProgram(program: Program, block: ProgramUse.() -> Unit) {
     ProgramUse.name = program.name //glUse
     ProgramUse.block()
-    glUseProgram(0)
+    GL20.glUseProgram(0)
 }
 
 inline fun withProgram(program: Int = 0, block: ProgramBase.() -> Unit) {
@@ -66,12 +47,12 @@ object ProgramUse {
 
     var name = 0
         set(value) {
-            glUseProgram(value)
+            GL20.glUseProgram(value)
             field = value
         }
 
     val String.location: Int
-        get() = glGetUniformLocation(name, this)
+        get() = GL20.glGetUniformLocation(name, this)
 
     var String.blockIndex
         get() = glGetUniformBlockIndex(name, this)
@@ -79,31 +60,31 @@ object ProgramUse {
 
     var Int.int: Int
         get() = 0
-        set(value) = glUniform1i(this, value)
+        set(value) = GL20.glUniform1i(this, value)
     var Int.float: Float
         get() = 0f
-        set(value) = glUniform1f(this, value)
+        set(value) = GL20.glUniform1f(this, value)
     var Int.vec2: Vec2
         get() = Vec2()
-        set(value) = glUniform2f(this, value.x, value.y)
+        set(value) = GL20.glUniform2f(this, value.x, value.y)
     var Int.vec3: Vec3
         get() = Vec3()
-        set(value) = glUniform3f(this, value.x, value.y, value.z)
+        set(value) = GL20.glUniform3f(this, value.x, value.y, value.z)
     var Int.vec4: Vec4
         get() = Vec4()
-        set(value) = glUniform4f(this, value.x, value.y, value.z, value.w)
+        set(value) = GL20.glUniform4f(this, value.x, value.y, value.z, value.w)
     var Int.mat4: Mat4
         get() = Mat4()
-        set(value) = glUniformMatrix4fv(this, false, value to mat4Buffer)
+        set(value) = GL20.glUniformMatrix4fv(this, false, value to mat4Buffer)
     var Int.mat3: Mat3
         get() = Mat3()
-        set(value) = glUniformMatrix3fv(this, false, value to mat3Buffer)
+        set(value) = GL20.glUniformMatrix3fv(this, false, value to mat3Buffer)
 
 
-    fun link() = glLinkProgram(name)
+    fun link() = GL20.glLinkProgram(name)
 
-    infix fun Vec4.to(location: Int) = glUniform4fv(location, this to vec4Buffer)
-    infix fun Mat4.to(location: Int) = glUniformMatrix4fv(location, false, this to mat4Buffer)
+    infix fun Vec4.to(location: Int) = GL20.glUniform4fv(location, this to vec4Buffer)
+    infix fun Mat4.to(location: Int) = GL20.glUniformMatrix4fv(location, false, this to mat4Buffer)
 }
 
 object ProgramBase {
@@ -111,8 +92,8 @@ object ProgramBase {
     var name = 0
 
     var String.location
-        get() = glGetUniformLocation(name, this)
-        set(value) = glBindAttribLocation(name, value, this)
+        get() = GL20.glGetUniformLocation(name, this)
+        set(value) = GL20.glBindAttribLocation(name, value, this)
 
     // read only because no program is used
     val String.blockIndex
@@ -121,16 +102,16 @@ object ProgramBase {
     inline fun use(block: ProgramUse.() -> Unit) {
         ProgramUse.name = name
         ProgramUse.block()
-        glUseProgram(0)
+        GL20.glUseProgram(0)
     }
 
     infix fun Int.blockBinding(uniformBlockBinding: Int) = glUniformBlockBinding(name, this, uniformBlockBinding)
     infix fun Int.getBlockIndex(uniformBlockName: String) = GL31.glGetUniformBlockIndex(this, uniformBlockName)
 
-    fun link() = glLinkProgram(name)
+    fun link() = GL20.glLinkProgram(name)
 
-    operator fun plusAssign(shader: Int) = glAttachShader(name, shader)
-    infix fun attach(shader: Int) = glAttachShader(name, shader)
+    operator fun plusAssign(shader: Int) = GL20.glAttachShader(name, shader)
+    infix fun attach(shader: Int) = GL20.glAttachShader(name, shader)
 }
 
 object Programs {
@@ -145,11 +126,12 @@ object Programs {
     fun using(index: Int, block: ProgramUse.() -> Unit) {
         ProgramUse.name = names[index]  // bind
         ProgramUse.block()
-        glUseProgram(0)
+        GL20.glUseProgram(0)
     }
 }
 
 
-fun glUseProgram() = glUseProgram(0)
+fun glUseProgram(program: Program) = GL20.glUseProgram(program.name)
+fun glUseProgram() = GL20.glUseProgram(0)
 
-fun glDeletePrograms(programs: IntArray) = programs.forEach { glDeleteProgram(it) }
+fun glDeletePrograms(programs: IntArray) = programs.forEach { GL20.glDeleteProgram(it) }
