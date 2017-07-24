290456db201dad2bcb9d98c032e5b0d6023097c2
Giuseppe Barbieri <elect86@gmail.com>
2017-07-24 18:18:54 +0200

    Char.isPrintable

--- src/main/kotlin/uno/kotlin/util.kt
+++ src/main/kotlin/uno/kotlin/util.kt
@@ -1,5 +1,6 @@
 package uno.kotlin
 
+import java.awt.event.KeyEvent
 import java.io.File
 
 /**
@@ -16,8 +17,19 @@ infix fun <T> (() -> Any).shallThrow(exClass: Class<T>) {
     throw Error("No exception")
 }
 
-val String.uri  get() = url.toURI()!!
-val String.url  get() = ClassLoader.getSystemResource(this)!!
-val String.stream  get() = ClassLoader.getSystemResourceAsStream(this)!!
+val String.uri get() = url.toURI()!!
+val String.url get() = ClassLoader.getSystemResource(this)!!
+val String.stream get() = ClassLoader.getSystemResourceAsStream(this)!!
 
 val String.file get() = File(uri)
+
+val Char.isPrintable: Boolean get() = with(Character.UnicodeBlock.of(this)) {
+    (!Character.isISOControl(this@isPrintable)) &&
+            this@isPrintable != KeyEvent.CHAR_UNDEFINED &&
+            this != null &&
+            this != Character.UnicodeBlock.SPECIALS
+}
+
+fun main(args: Array<String>) {
+    println('2'.isPrintable)
+}
