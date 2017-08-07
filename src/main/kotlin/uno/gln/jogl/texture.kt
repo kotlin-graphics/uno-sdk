13c0af0dd45f68f0c2a907eac872fb47c346c70d
Giuseppe Barbieri <elect86@gmail.com>
2017-08-07 14:27:39 +0200

    jogl gln filters

--- src/main/kotlin/uno/gln/jogl/texture.kt
+++ src/main/kotlin/uno/gln/jogl/texture.kt
@@ -3,9 +3,12 @@ package uno.gln.jogl
 import com.jogamp.opengl.GL
 import com.jogamp.opengl.GL2ES3
 import com.jogamp.opengl.GL2GL3
+import com.jogamp.opengl.GL2GL3.*
 import com.jogamp.opengl.GL3
 import glm_.set
+import glm_.vec2.Vec2i
 import uno.gln.intBuffer
+import java.nio.ByteBuffer
 import java.nio.IntBuffer
 
 fun GL3.glGenTextures(textures: IntBuffer) = glGenTextures(textures.capacity(), textures)
@@ -199,32 +202,88 @@ object Texture2d {
     lateinit var gl: GL3
     var name = 0
         set(value) {
-            gl.glBindTexture(GL2GL3.GL_TEXTURE_2D, value)
+            gl.glBindTexture(GL_TEXTURE_2D, value)
             field = name
         }
 
-    fun image(internalFormat: Int, width: Int, height: Int, format: Int, type: Int, pixels: java.nio.Buffer) =
-            gl.glTexImage2D(GL2GL3.GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, type, pixels)
+    fun image(internalFormat: Int, width: Int, height: Int, format: Int, type: Int, pixels: ByteBuffer) =
+            gl.glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, type, pixels)
 
-    fun image(level: Int, internalFormat: Int, width: Int, height: Int, format: Int, type: Int, pixels: java.nio.Buffer) =
-            gl.glTexImage2D(GL2GL3.GL_TEXTURE_2D, level, internalFormat, width, height, 0, format, type, pixels)
+    fun image(level: Int, internalFormat: Int, width: Int, height: Int, format: Int, type: Int, pixels: ByteBuffer) =
+            gl.glTexImage2D(GL_TEXTURE_2D, level, internalFormat, width, height, 0, format, type, pixels)
+
+    // TODO size for others
+    fun image(internalFormat: Int, size: Vec2i, format: Int, type: Int, pixels: ByteBuffer) =
+            gl.glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, size.x, size.y, 0, format, type, pixels)
+
+    fun image(level: Int, internalFormat: Int, size: Vec2i, format: Int, type: Int, pixels: ByteBuffer) =
+            gl.glTexImage2D(GL_TEXTURE_2D, level, internalFormat, size.x, size.y, 0, format, type, pixels)
+
+    fun storage(internalFormat: Int, size: Vec2i) = storage(1, internalFormat, size)
+    fun storage(levels: Int, internalFormat: Int, size: Vec2i) =
+            gl.glTexStorage2D(GL_TEXTURE_2D, levels, internalFormat, size.x, size.y)
 
     var baseLevel = 0
         set(value) {
-            gl.glTexParameteri(GL2GL3.GL_TEXTURE_2D, GL2ES3.GL_TEXTURE_BASE_LEVEL, value)
+            gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, value)
             field = value
         }
     var maxLevel = 1_000
         set(value) {
-            gl.glTexParameteri(GL2GL3.GL_TEXTURE_2D, GL2ES3.GL_TEXTURE_MAX_LEVEL, value)
+            gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, value)
             field = value
         }
     var levels = 0..1_000
         set(value) {
-            gl.glTexParameteri(GL2GL3.GL_TEXTURE_1D, GL2ES3.GL_TEXTURE_BASE_LEVEL, value.first)
-            gl.glTexParameteri(GL2GL3.GL_TEXTURE_1D, GL2ES3.GL_TEXTURE_MAX_LEVEL, value.endInclusive)
+            gl.glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_BASE_LEVEL, value.first)
+            gl.glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAX_LEVEL, value.endInclusive)
             field = value
         }
+
+    val linear = Filer.linear
+    val nearest = Filer.nearest
+
+    val nearest_mmNearest = Filer.nearest_mmNearest
+    val linear_mmNearest = Filer.linear_mmNearest
+    val nearest_mmLinear = Filer.nearest_mmLinear
+    val linear_mmLinear = Filer.linear_mmLinear
+
+    val clampToEdge = Wrap.clampToEdge
+    val mirroredRepeat = Wrap.mirroredRepeat
+    val repeat = Wrap.repeat
+
+    var magFilter = linear
+        set(value) {
+            gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, value.i)
+            field = value
+        }
+    var minFilter = nearest_mmLinear
+        set(value) {
+            gl.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, value.i)
+            field = value
+        }
+    //    var maxAnisotropy = 1.0f
+//        set(value) {
+//            glTexParameteri(name, EXTTextureFilterAnisotropic.GL_TEXTURE_MAX_ANISOTROPY_EXT, value)
+//            field = value
+//        }
+    var wrapS = repeat
+        set(value) {
+            gl.glTexParameteri(name, GL_TEXTURE_WRAP_S, value.i)
+            field = value
+        }
+    var wrapT = repeat
+        set(value) {
+            gl.glTexParameteri(name, GL_TEXTURE_WRAP_T, value.i)
+            field = value
+        }
+
+    enum class Filer(val i: Int) {nearest(GL_NEAREST), linear(GL_LINEAR),
+        nearest_mmNearest(GL_NEAREST_MIPMAP_NEAREST), linear_mmNearest(GL_LINEAR_MIPMAP_NEAREST),
+        nearest_mmLinear(GL_NEAREST_MIPMAP_LINEAR), linear_mmLinear(GL_LINEAR_MIPMAP_LINEAR)
+    }
+
+    enum class Wrap(val i: Int) {clampToEdge(GL_CLAMP_TO_EDGE), mirroredRepeat(GL_MIRRORED_REPEAT), repeat(GL_REPEAT) }
 }
 
 object Textures {
