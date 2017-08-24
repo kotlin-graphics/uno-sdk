143c1eda3a88ab20f11f5c26533cdc1eb19888b4
gBarbieri <elect86@gmail.com>
2017-08-24 17:44:47 +0200

    gln glGenRenderbuffers

--- src/main/kotlin/uno/gln/renderbuffer.kt
+++ src/main/kotlin/uno/gln/renderbuffer.kt
@@ -1,12 +1,13 @@
 package uno.gln
 
 import glm_.vec2.Vec2i
+import org.lwjgl.opengl.GL11
 import org.lwjgl.opengl.GL30
-import org.lwjgl.opengl.GL30.GL_RENDERBUFFER
+import org.lwjgl.opengl.GL30.*
 import java.nio.IntBuffer
 import org.lwjgl.system.MemoryStack.stackPush
 import org.lwjgl.system.MemoryStack
-
+import java.nio.ByteBuffer
 
 
 /**
@@ -16,3 +17,34 @@ import org.lwjgl.system.MemoryStack
 fun glRenderbufferStorage(internalFormat: Int, width: Int, height: Int) = GL30.glRenderbufferStorage(GL_RENDERBUFFER, internalFormat, width, height)
 fun glRenderbufferStorage(internalFormat: Int, size: Vec2i) = GL30.glRenderbufferStorage(GL_RENDERBUFFER, internalFormat, size.x, size.y)
 fun glRenderbufferStorage(target:Int, internalFormat: Int, size: Vec2i) = GL30.glRenderbufferStorage(target, internalFormat, size.x, size.y)
+
+fun glGenRenderbuffers(renderbuffers: IntBuffer, block: RenderBuffers.() -> Unit) {
+    glGenRenderbuffers(renderbuffers)
+//    RenderBuffers.target = GL_RENDERBUFFER TODO?
+    RenderBuffers.names = renderbuffers
+    RenderBuffers.block()
+}
+
+object RenderBuffers {
+
+//    var target = 0
+    lateinit var names: IntBuffer
+
+    fun at(index: Int, block: RenderBuffer.() -> Unit) {
+        RenderBuffer.name = names[index] // bind
+        RenderBuffer.block()
+    }
+}
+
+object RenderBuffer {
+
+    var target = 0
+    var name = 0
+        set(value) {
+            glBindRenderbuffer(GL_RENDERBUFFER, value)
+            field = name
+        }
+
+    fun storage(internalFormat: Int, width: Int, height: Int) = glRenderbufferStorage(GL_RENDERBUFFER, internalFormat, width, height)
+    fun storage(internalFormat: Int, size: Vec2i) = glRenderbufferStorage(GL_RENDERBUFFER, internalFormat, size.x, size.y)
+}
