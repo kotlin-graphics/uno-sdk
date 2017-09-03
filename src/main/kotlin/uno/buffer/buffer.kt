354b8a0c1e65f218f618e00b9b3c7cd5b5d98efc
elect <elect86@gmail.com>
2017-09-03 21:35:54 +0200

    glf vertex

--- src/main/kotlin/uno/buffer/buffer.kt
+++ src/main/kotlin/uno/buffer/buffer.kt
@@ -7,6 +7,7 @@ import glm_.vec2.Vec2
 import glm_.vec3.Vec3
 import glm_.vec4.Vec4
 import org.lwjgl.system.MemoryUtil
+import uno.glf.glf
 import uno.kotlin.Quadruple
 import uno.kotlin.Quintuple
 import java.nio.*
@@ -168,6 +169,11 @@ fun bufferOf(vertices: Collection<*>): ByteBuffer {
         Vec4 -> {
             res = bufferBig(Vec4.size * vertices.size)
             for (i in 0 until vertices.size) (vertices as Collection<Vec4>).elementAt(i).to(res, i * Vec4.size)
+        }
+        glf.pos3_col4ub -> {
+            res = bufferBig(glf.pos3_col4ub.stride * vertices.size)
+            for (i in 0 until vertices.size)
+                (vertices as Collection<glf.pos3_col4ub>).elementAt(i).to(res, i * glf.pos3_col4ub.stride)
         }
         else -> throw Error()
     }
--- src/main/kotlin/uno/glf/vertex.kt
+++ src/main/kotlin/uno/glf/vertex.kt
@@ -10,6 +10,8 @@ import org.lwjgl.opengl.GL11
 import glm_.glm
 import glm_.vec4.Vec4b
 import org.lwjgl.opengl.GL11.*
+import java.nio.ByteBuffer
+import java.nio.FloatBuffer
 
 /**
  * Created by GBarbieri on 29.03.2017.
@@ -124,6 +126,124 @@ object glf {
     }
 
     object pos3_nor3_col4 : VertexLayout {
+        val stride = Vec3.size * 2 + Vec4.size
+        override var attribute = arrayOf(
+                VertexAttribute(semantic.attr.POSITION, Vec3.length, GL_FLOAT, false, stride, 0),
+                VertexAttribute(semantic.attr.NORMAL, Vec3.length, GL_FLOAT, false, stride, Vec3.size.L),
+                VertexAttribute(semantic.attr.COLOR, Vec4.length, GL_FLOAT, false, stride, Vec3.size.L * 2))
+    }
+
+}
+object Vertex { // TODO
+
+    class pos2 : VertexLayout {
+        val stride = Vec2.size
+        override var attribute = arrayOf(
+                VertexAttribute(semantic.attr.POSITION, Vec2.length, GL_FLOAT, false, stride, 0))
+    }
+
+    class pos3 : VertexLayout {
+        val stride = Vec3.size
+        override var attribute = arrayOf(
+                VertexAttribute(semantic.attr.POSITION, Vec3.length, GL_FLOAT, false, stride, 0))
+    }
+
+    class pos4 : VertexLayout {
+        val stride = Vec4.size
+        override var attribute = arrayOf(
+                VertexAttribute(semantic.attr.POSITION, Vec4.length, GL_FLOAT, false, stride, 0))
+    }
+
+    class pos3_col4 : VertexLayout {
+        val stride = Vec3.size + Vec4.size
+        override var attribute = arrayOf(
+                VertexAttribute(semantic.attr.POSITION, Vec3.length, GL_FLOAT, false, stride, 0),
+                VertexAttribute(semantic.attr.COLOR, Vec4.length, GL_FLOAT, false, stride, Vec3.size.L))
+    }
+
+    class pos3_col4b : VertexLayout {
+        val stride = Vec3.size + Vec4b.size
+        override var attribute = arrayOf(
+                VertexAttribute(semantic.attr.POSITION, Vec3.length, GL_FLOAT, false, stride, 0),
+                VertexAttribute(semantic.attr.COLOR, Vec4b.length, GL_UNSIGNED_BYTE, false, stride, Vec3.size.L))
+    }
+
+    class pos4_col4 : VertexLayout {
+        val stride = Vec4.size * 2
+        override var attribute = arrayOf(
+                VertexAttribute(semantic.attr.POSITION, Vec4.length, GL_FLOAT, false, stride, 0),
+                VertexAttribute(semantic.attr.COLOR, Vec4.length, GL_FLOAT, false, stride, Vec4.size.L))
+    }
+
+
+    class pos2_tc2 : VertexLayout {
+        val stride = Vec2.size * 2
+        override var attribute = arrayOf(
+                VertexAttribute(semantic.attr.POSITION, Vec2.length, GL_FLOAT, false, stride, 0),
+                VertexAttribute(semantic.attr.TEX_COORD, Vec2.length, GL_FLOAT, false, stride, Vec2.size.L))
+    }
+
+    class pos2_tc2_col4b : VertexLayout {
+        val stride = Vec2.size * 2 + Vec4b.size
+        override var attribute = arrayOf(
+                VertexAttribute(semantic.attr.POSITION, Vec2.length, GL_FLOAT, false, stride, 0),
+                VertexAttribute(semantic.attr.TEX_COORD, Vec2.length, GL_FLOAT, false, stride, Vec2.size.L),
+                VertexAttribute(semantic.attr.COLOR, Vec4b.length, GL_FLOAT, false, stride, Vec2.size.L * 2))
+    }
+
+    class pos2us_tc2us : VertexLayout {
+        val stride = Vec2us.size * 2
+        override var attribute = arrayOf(
+                VertexAttribute(semantic.attr.POSITION, Vec2us.length, GL_UNSIGNED_SHORT, false, stride, 0),
+                VertexAttribute(semantic.attr.TEX_COORD, Vec2us.length, GL_UNSIGNED_SHORT, false, stride, Vec2us.size.L))
+    }
+
+    class pos3_tc2 : VertexLayout {
+        val stride = Vec3.size + Vec2.size
+        override var attribute = arrayOf(
+                VertexAttribute(semantic.attr.POSITION, Vec3.length, GL_FLOAT, false, stride, 0),
+                VertexAttribute(semantic.attr.TEX_COORD, Vec2.length, GL_FLOAT, false, stride, Vec3.size.L))
+    }
+
+    class pos3_col4ub(var p: Vec3, var c: Vec4ub) {
+        infix fun to(buffer: ByteBuffer) = to(buffer, buffer.position())
+        fun to(buffer: ByteBuffer, index: Int): ByteBuffer {
+            p.to(buffer, index)
+            c.to(buffer, index + Vec3.size)
+            return buffer
+        }
+    }
+
+    class pos2_tc3 : VertexLayout {
+        val stride = Vec2.size + Vec3.size
+        override var attribute = arrayOf(
+                VertexAttribute(semantic.attr.POSITION, Vec2.length, GL_FLOAT, false, stride, 0),
+                VertexAttribute(semantic.attr.TEX_COORD, Vec3.length, GL_FLOAT, false, stride, Vec2.size.L))
+    }
+
+    class pos3_tc3 : VertexLayout {
+        val stride = Vec3.size * 2
+        override var attribute = arrayOf(
+                VertexAttribute(semantic.attr.POSITION, Vec3.length, GL_FLOAT, false, stride, 0),
+                VertexAttribute(semantic.attr.TEX_COORD, Vec3.length, GL_FLOAT, false, stride, Vec3.size.L))
+    }
+
+    class pos3_nor3 : VertexLayout {
+        val stride = Vec3.size * 2
+        override var attribute = arrayOf(
+                VertexAttribute(semantic.attr.POSITION, Vec3.length, GL_FLOAT, false, stride, 0),
+                VertexAttribute(semantic.attr.NORMAL, Vec3.length, GL_FLOAT, false, stride, Vec3.size.L))
+    }
+
+    class pos3_nor3_tc2 : VertexLayout {
+        val stride = Vec3.size * 2 + Vec2.size
+        override var attribute = arrayOf(
+                VertexAttribute(semantic.attr.POSITION, Vec3.length, GL_FLOAT, false, stride, 0),
+                VertexAttribute(semantic.attr.NORMAL, Vec3.length, GL_FLOAT, false, stride, Vec3.size.L),
+                VertexAttribute(semantic.attr.TEX_COORD, Vec2.length, GL_FLOAT, false, stride, Vec3.size.L * 2))
+    }
+
+    class pos3_nor3_col4 : VertexLayout {
         val stride = Vec3.size * 2 + Vec4.size
         override var attribute = arrayOf(
                 VertexAttribute(semantic.attr.POSITION, Vec3.length, GL_FLOAT, false, stride, 0),
