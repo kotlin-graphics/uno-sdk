251298f7fd3e39d3a7f986bacc602bd9ffe8d385
elect86 <elect86@gmail.com>
2018-03-17 18:31:51 +0100

    caps test

---
+++ src/test/kotlin/uno/caps.kt
@@ -0,0 +1,32 @@
+package uno
+
+import io.kotlintest.specs.StringSpec
+import org.lwjgl.opengl.GL
+import uno.caps.Caps
+import uno.glfw.GlfwWindow
+import uno.glfw.glfw
+
+class caps : StringSpec() {
+
+    init {
+
+        "caps" {
+
+            with(glfw) {
+                init()
+                windowHint {
+                    context.version = "3.3"
+                    profile = "core"
+                }
+            }
+
+            GlfwWindow(640, "title").apply { makeContextCurrent() }
+
+            GL.createCapabilities()
+
+            val caps = Caps(Caps.Profile.CORE)
+
+            println()
+        }
+    }
+}
