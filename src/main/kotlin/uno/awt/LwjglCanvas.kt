91c578626d058d05b094a3e6260114aa253f520b
Florian Schäfers <florian.schaefers@gmail.com>
2019-03-25 15:54:52 +0100

    - added animate method to start viewer animation

--- src/main/kotlin/uno/awt/LwjglCanvas.kt
+++ src/main/kotlin/uno/awt/LwjglCanvas.kt
@@ -15,6 +15,9 @@ import java.awt.Graphics
 import java.awt.event.ComponentAdapter
 import java.awt.event.ComponentEvent
 import javax.swing.SwingUtilities
+import java.util.logging.Level.SEVERE
+import java.lang.reflect.InvocationTargetException
+
 
 /**
  * A Canvas component that uses OpenGL for rendering.
@@ -297,6 +300,39 @@ abstract class LwjglCanvas(val glDebug: Boolean = false) : Canvas() {
         else {
             animated = true
             repaint()
+        }
+    }
+
+    fun animate(animate: Boolean = true) {
+
+        if (animated != animate) {
+
+            animated = animate
+
+            if (animate) {
+
+                if (SwingUtilities.isEventDispatchThread()) {
+                    val g = graphics
+                    if (g != null) {
+                        paint(g)
+                        g.dispose()
+                    }
+                } else {
+                    try {
+                        SwingUtilities.invokeAndWait {
+                            val g = graphics
+                            if (g != null) {
+                                paint(g)
+                                g.dispose()
+                            }
+                        }
+                    } catch (ex: InterruptedException) {
+                        println("Exception while starting viewer animation")
+                    } catch (ex: InvocationTargetException) {
+                        println("Exception while starting viewer animation")
+                    }
+                }
+            }
         }
     }
 
