d491052e7b48766b9c22c16a63d3a809070460c5
gbarbieri <elect86@gmail.com>
2019-07-12 17:25:52 +0200

    broken, JPMS in progress

---
+++ uno-core/src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module com.github.kotlin_graphics.uno_core {
+
+    requires kotlin.stdlib;
+
+    requires org.lwjgl;
+
+    requires com.github.kotlin_graphics.gln;
+    requires com.github.kotlin_graphics.gli;
+    requires com.github.kotlin_graphics.glm;
+    requires com.github.kotlin_graphics.kool;
+    requires com.github.kotlin_graphics.kotlin_unsigned;
+
+    exports uno.buffer;
+    exports uno.convert;
+    exports uno.glfw;
+    exports uno.glm;
+    exports uno.kotlin;
+//    exports uno.mousePole;
+    exports uno.stb;
+    exports uno.time;
+}
