5ae1ab94cc249e017c41251d897949ff85d331b3
gbarbieri <elect86@gmail.com>
2019-07-16 10:17:47 +0200

    general bump up build.gradle files simplified

--- uno-gl/src/main/java/module-info.java
+++ uno-gl/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.github.kotlin_graphics.uno_awt {
 
     requires kotlin.stdlib;
 
+    requires com.github.kotlin_graphics.glm;
+
 //    exports uno.debug;
     exports uno.gl;
 //    exports uno.glsl;
--- uno-vk/src/main/java/module-info.java
+++ uno-vk/src/main/java/module-info.java
@@ -2,5 +2,7 @@ module com.github.kotlin_graphics.uno_vk {
 
     requires kotlin.stdlib;
 
+    requires com.github.kotlin_graphics.kool;
+
     exports uno;
 }
