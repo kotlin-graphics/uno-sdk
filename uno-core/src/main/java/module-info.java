cb9073be566b5ca09e3a6d2a8edfb661a721c751
gbarbieri <elect86@gmail.com>
2019-07-18 14:06:09 +0200

    jvmTarget = "11"

--- uno-core/src/main/java/module-info.java
+++ uno-core/src/main/java/module-info.java
@@ -11,6 +11,8 @@ module com.github.kotlin_graphics.uno_core {
     requires com.github.kotlin_graphics.glm;
     requires com.github.kotlin_graphics.kool;
     requires com.github.kotlin_graphics.kotlin_unsigned;
+    requires org.lwjgl.glfw;
+    requires org.lwjgl.opengl;
 
     exports uno.buffer;
     exports uno.convert;
--- uno-vk/src/main/java/module-info.java
+++ uno-vk/src/main/java/module-info.java
@@ -2,6 +2,12 @@ module com.github.kotlin_graphics.uno_vk {
 
     requires kotlin.stdlib;
 
+    requires org.lwjgl;
+    requires org.lwjgl.vulkan;
+    requires org.lwjgl.glfw;
+
+    requires com.github.kotlin_graphics.uno_core;
+    requires com.github.kotlin_graphics.vkk;
     requires com.github.kotlin_graphics.kool;
 
     exports uno;
