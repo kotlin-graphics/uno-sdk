72fb1bd171eba9074579739dfd8aefddfefd854a
gbarbieri <elect86@gmail.com>
2019-08-26 11:31:06 +0200

    adr

--- uno-core/src/main/kotlin/uno/glfw/direct fields.kt
+++ uno-core/src/main/kotlin/uno/glfw/direct fields.kt
@@ -2,23 +2,24 @@ package uno.glfw
 
 import glm_.vec2.Vec2i
 import kool.Stack
+import kool.adr
 import org.lwjgl.glfw.GLFWVidMode
 
 
 inline val GLFWVidMode.width: Int
-    get() = GLFWVidMode.nwidth(address()) // TODO adr
+    get() = GLFWVidMode.nwidth(adr)
 inline val GLFWVidMode.height: Int
-    get() = GLFWVidMode.nheight(address())
+    get() = GLFWVidMode.nheight(adr)
 inline val GLFWVidMode.size: Vec2i
     get() = Vec2i(width, height)
 inline val GLFWVidMode.redBits: Int
-    get() = GLFWVidMode.nredBits(address())
+    get() = GLFWVidMode.nredBits(adr)
 inline val GLFWVidMode.greenBits: Int
-    get() = GLFWVidMode.ngreenBits(address())
+    get() = GLFWVidMode.ngreenBits(adr)
 inline val GLFWVidMode.blueBits: Int
-    get() = GLFWVidMode.nblueBits(address())
+    get() = GLFWVidMode.nblueBits(adr)
 inline val GLFWVidMode.refreshRate: Int
-    get() = GLFWVidMode.nrefreshRate(address())
+    get() = GLFWVidMode.nrefreshRate(adr)
 
 inline class HWND(val L: Long)
 
