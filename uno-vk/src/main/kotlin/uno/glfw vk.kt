413fb15d30f205c558bfdf74a33bae99790cade1
elect86 <elect86@gmail.com>
2019-11-03 14:50:16 +0100

    GlfwWindow.createSurface

--- uno-vk/src/main/kotlin/uno/glfw vk.kt
+++ uno-vk/src/main/kotlin/uno/glfw vk.kt
@@ -4,13 +4,14 @@ import kool.adr
 import org.lwjgl.glfw.GLFWVulkan
 import org.lwjgl.system.MemoryUtil
 import org.lwjgl.vulkan.VkInstance
+import uno.glfw.GlfwWindow
 import uno.glfw.GlfwWindowHandle
 import uno.glfw.glfw
 import uno.glfw.stak
 import vkk.VK_CHECK_RESULT
 import vkk.entities.VkSurfaceKHR
 
-fun glfw.createWindowSurface(instance: VkInstance, windowHandle: GlfwWindowHandle): VkSurfaceKHR =
-        VkSurfaceKHR(stak.longAddress { surface ->
-            VK_CHECK_RESULT(GLFWVulkan.nglfwCreateWindowSurface(instance.adr, windowHandle.L, MemoryUtil.NULL, surface))
+infix fun GlfwWindow.createSurface(instance: VkInstance): VkSurfaceKHR =
+        VkSurfaceKHR(stak.longAddress {
+            VK_CHECK_RESULT(GLFWVulkan.nglfwCreateWindowSurface(instance.adr, handle.L, MemoryUtil.NULL, it))
         })
