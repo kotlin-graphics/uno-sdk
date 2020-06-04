6b834083d2714d0795c4bbf867f0202c406b0469
gbarbieri <elect86@gmail.com>
2020-06-04 16:57:20 +0200

    fixing Actions

--- uno-gl/src/test/kotlin/uno/helloWorld.kt
+++ uno-gl/src/test/kotlin/uno/helloWorld.kt
@@ -1,35 +1,35 @@
 package uno
 
-import io.kotlintest.specs.StringSpec
+import io.kotest.core.spec.style.StringSpec
 import org.lwjgl.opengl.GL11.*
 import uno.glfw.GlfwWindow
 import uno.glfw.glfw
-import uno.glfw.size
+import uno.kotlin.isNotCI
 
 
 class HelloWorld : StringSpec() {
 
     init {
+        if (isNotCI)
+            "Hello World" {
 
-        "Hello World" {
+                // init
+                glfw.init("3.3")
 
-            // init
-            glfw.init("3.3")
+                // The window handle
+                GlfwWindow(1280, 720, "ImGui Lwjgl OpenGL3 example").apply {
 
-            // The window handle
-            GlfwWindow(1280, 720, "ImGui Lwjgl OpenGL3 example").apply {
+                    pos = (glfw.primaryMonitor.videoMode.size - size) / 2
 
-                pos = (glfw.primaryMonitor.videoMode.size - size) / 2
+                    init()
 
-                init()
+                    // Set the clear color
+                    glClearColor(1f, 0f, 0f, 0f)
 
-                // Set the clear color
-                glClearColor(1f, 0f, 0f, 0f)
-
-                loop({ glfw.time < 1 }) {
-                    glClear(GL_COLOR_BUFFER_BIT)
+                    loop({ glfw.time < 1 }) {
+                        glClear(GL_COLOR_BUFFER_BIT)
+                    }
                 }
             }
-        }
     }
 }
