f8ca22cf0e73dfc705dfbe0d724022d3a843b896
Giuseppe Barbieri <elect86@gmail.com>
2020-09-09 09:44:27 +0200

    v0.7.9

--- uno-core/src/main/kotlin/uno/kotlin/util.kt
+++ uno-core/src/main/kotlin/uno/kotlin/util.kt
@@ -77,7 +77,7 @@ operator fun IntBuffer.plusAssign(i: Int) {
 
 const val NUL = '\u0000'
 
-val version = "0.7.8"
+val version = "0.7.9"
 
 val isNotCI: Boolean
     get() = System.getenv("GITHUB_ACTIONS") != "true" && System.getenv("TRAVIS") != "true"
