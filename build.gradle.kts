12af6f8e9a4fa3436a30439631b925fe17892c28
Giuseppe Barbieri <elect86@gmail.com>
2021-03-29 17:11:49 +0200

    v0.7.9+33

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,51 +4,21 @@ import kx.kxImplementation
 import kx.lwjglImplementation
 
 plugins {
-    val build = "0.7.0+84"
-    id("kx.kotlin.11") version build apply false
+    val build = "0.7.0+88"
+    id("kx.kotlin.11") version build
     id("kx.lwjgl") version build apply false
     id("kx.dokka") version build apply false
     id("kx.dokka.multimodule") version build
-    //    id("kx.publish.multimodule") version build
+    id("kx.publish") version build
     //    java
-    id("kx.snapshot") version "0.0.5"
 }
 
-version = "0.7.9+29" // for ::bump
+version = "0.7.9+33" // for ::bump
 
 subprojects {
-    apply(plugin = "kx.kotlin.11")
     apply(plugin = "kx.lwjgl")
     apply(plugin = "kx.dokka")
-    //    apply(plugin = "kx.publish")
     apply(plugin = "java")
-
-    version = rootProject.version
-    group = "kotlin.graphics"
-
-    apply(plugin = "maven-publish")
-
-    tasks {
-        // this is needed because we have a separate compile step in this example with the 'module-info.java' is in 'main/java' and the Kotlin code is in 'main/kotlin'
-        named<JavaCompile>("compileJava") {
-            val module = project.run { "$group.uno.$name" }
-            options.compilerArgs = listOf("--patch-module", "$module=${sourceSets.main.get().output.asPath}")
-        }
-    }
-
-    // limited dsl support inside here
-    extensions.configure<PublishingExtension>("publishing") {
-        publications.create<MavenPublication>("maven") {
-            artifactId = "uno-${project.name}"
-            from(components["java"])
-            suppressPomMetadataWarningsFor("runtimeElements")
-        }
-        repositories {
-            maven {
-                url = uri("$rootDir/mary")
-            }
-        }
-    }
 }
 
 project(":core") {
